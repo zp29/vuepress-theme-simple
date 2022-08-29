28581fa69dd66c9c267891949d8e802bae8eb61b
Zheng <823476872@qq.com>
2022-08-29 15:46:05 +0800

    Update FooterBar.vue

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -1,36 +1,24 @@
 <template>
-  <footer class="footer">
-    © {{ thisYear }}
-    <i
-      v-if="author"
-      class="icon-user"
-    />
-    {{ author }}
-    <br>
-    Powered by
-    <a
-      href="https://vuepress.vuejs.org/"
-      rel="noopener"
-      target="_blank"
-    >VuePress</a>
-    | Theme
-    <a
-      href="https://github.com/viko16/vuepress-theme-simple"
-      rel="noopener"
-      target="_blank"
-    >Simple</a>
-  </footer>
-</template>
-
-<script>
-export default {
-  computed: {
-    thisYear() {
-      return new Date().getFullYear();
-    },
-    author() {
-      return this.$themeConfig.author || '';
-    },
+    <footer class="footer">
+      © {{ thisYear }}
+      <i
+        v-if="author"
+        class="icon-user"
+      />
+      {{ author }}
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      thisYear() {
+        return this.$themeConfig.startYear || '';
+      },
+      author() {
+        return this.$themeConfig.author || '';
+      },
+    }
   }
-}
-</script>
+  </script>
+  
